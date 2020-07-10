03149a67ad9fb0a46e2aa121f441f00a2097cec8
JackR1 <>
2020-07-10 18:33:08 +1000

    Initial commit.

---
+++ SiriRemoteVoiceDecoder/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SiriRemoteVoiceDecoder
+//
+//  Created by Jack on 10/7/20.
+//  Copyright © 2020 JackR1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
